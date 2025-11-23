69456975586abfb5580eb9beb3a6049f2686f9dd
efekaanaltas <efekaanaltas@yandex.com>
2025-11-23 20:41:30 +0300

    Initial commit

---
+++ src/scene.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+
+export function createScene() {
+    const gameWindow = document.getElementById('render-target');
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xff77ff);
+
+    const camera = new THREE.PerspectiveCamera(75, gameWindow.offsetWidth / gameWindow.offsetHeight, 0.1, 1000);
+    camera.position.z = 5;
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(gameWindow.offsetWidth, gameWindow.offsetHeight);
+    gameWindow.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1,1,1);
+    const material = new THREE.MeshBasicMaterial({color: 0xffffaa});
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    function draw() {
+        renderer.render(scene, camera);
+    }
+
+    function start() {
+        renderer.setAnimationLoop(draw);
+    }
+
+    function stop() {
+        renderer.setAnimationLoop(null);
+    }
+
+    return {
+        start,
+        stop
+    }
+}
